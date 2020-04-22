8517caf6d67a3adbb15cd54157e3bad8571fcafe
Axel Venet <kern046@gmail.com>
2020-04-23 01:07:04 +0200

    Implement JWT authentication

--- src/menu_screen.gd
+++ src/menu_screen.gd
@@ -1,17 +1,25 @@
 extends Control
 
 var lobby_card_scene = preload("res://lobby_card.tscn")
+var token = ""
 
 func _ready():
 	get_node("Body/Footer/LobbyCreationButton").connect("button_down", self, "create_lobby")
 	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
-	get_lobbies()
+	auth()
+	
+func auth():
+	$HTTPRequest.request("http://127.0.0.1:8080/login", [], false, HTTPClient.METHOD_POST)
 	
 func get_lobbies():
-	$HTTPRequest.request("http://127.0.0.1:8080/lobbies/")
+	$HTTPRequest.request("http://127.0.0.1:8080/api/lobbies/", [
+		"Authorization: Bearer " + token
+	])
 	
 func create_lobby():
-	$HTTPRequest.request("http://127.0.0.1:8080/lobbies/", [], false, HTTPClient.METHOD_POST)
+	$HTTPRequest.request("http://127.0.0.1:8080/api/lobbies/", [
+		"Authorization: Bearer " + token
+	], false, HTTPClient.METHOD_POST)
 	
 func add_lobby_cards(lobbies):
 	for lobby in lobbies:
@@ -25,6 +33,10 @@ func add_lobby_card(lobby):
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
 	if response_code == 200:
-		add_lobby_cards(json.result)
+		if json.result.has('token'):
+			token = json.result.token
+			get_lobbies()
+		else:
+			add_lobby_cards(json.result)
 	elif response_code == 201:
 		add_lobby_card(json.result)
