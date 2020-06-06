179ad179dd38a48c6b9909543019723f78d27678
ChickenStorm <tetras.aigle@yahoo.fr>
2020-06-06 19:11:34 +0200

    fix pin superposition

--- game/map/system.gd
+++ game/map/system.gd
@@ -6,10 +6,11 @@ var scale_ratio = 1.0
 var system_fleet_pin_scene = preload("res://game/map/system_fleet_pin.tscn")
 var is_in_range_sailing_fleet = false
 var _time = 0.0
-var _previous_alpha = 1.0
+var _alpha = 1.0
 
-const ALPHA_SPEED_GAIN = 2 #in alpha per second^2
+const ALPHA_SPEED_GAIN = 2.0
 const SNAP_ALPHA_DISTANCE = 0.05
+const ALPHA_APLITUDE = 0.4
 
 func _ready():
 	set_position(Vector2(system.coordinates.x * 50, system.coordinates.y * 50))
@@ -27,12 +28,12 @@ func _process(delta):
 	_time += delta
 	var target_alpha = 1.0
 	if is_in_range_sailing_fleet:
-		target_alpha = cos (_time * PI ) * 0.4 +0.6
-	if _previous_alpha != target_alpha:
-		_previous_alpha = max(min( (target_alpha-_previous_alpha)*ALPHA_SPEED_GAIN * delta + _previous_alpha,1.0),0.0)
-		if abs(target_alpha-_previous_alpha)<SNAP_ALPHA_DISTANCE:
-			_previous_alpha=target_alpha
-		_modulate_color(_previous_alpha)
+		target_alpha = cos (_time * PI ) * ALPHA_APLITUDE + (1.0-ALPHA_APLITUDE)
+	if _alpha != target_alpha:
+		_alpha = max(min( (target_alpha - _alpha) * ALPHA_SPEED_GAIN * delta + _alpha,1.0),0.0)
+		if abs(target_alpha-_alpha)<SNAP_ALPHA_DISTANCE:
+			_alpha=target_alpha
+		_modulate_color(_alpha)
 
 func _on_fleet_selected(fleet):
 	is_in_range_sailing_fleet = Store.is_in_range(fleet,system)
@@ -54,17 +55,22 @@ func unselect():
 		$Star.set_scale(Vector2(scale_ratio, scale_ratio))
 		
 func refresh_fleet_pins():
-	var factions = []
+	var is_current_player_included = false
+	var is_another_player_included = false
 	for pin in $FleetPins.get_children(): pin.queue_free()
 	for fleet in system.fleets.values():
 		var p = Store.get_game_player(fleet.player)
-		if !factions.has(p.faction):
-			factions.push_back(p.faction)
-			add_fleet_pin(p.faction, Store.get_faction(p.faction).color)
+		if !is_current_player_included && p.id == Store._state.player.id:
+			is_current_player_included = true
+			add_fleet_pin(p, Store.get_faction(p.faction).color)
+		elif !is_another_player_included && p.id != Store._state.player.id:
+			is_another_player_included = true
+			add_fleet_pin(p, Store.get_faction(p.faction).color)
 		
-func add_fleet_pin(faction, color):
+func add_fleet_pin(player, color):
 	var fleet_pin = system_fleet_pin_scene.instance()
-	fleet_pin.faction = faction
+	fleet_pin.faction = player.faction
+	fleet_pin.is_current_player = (player.id == Store._state.player.id)
 	fleet_pin.color = color
 	$FleetPins.add_child(fleet_pin)
 
--- game/map/system_fleet_pin.gd
+++ game/map/system_fleet_pin.gd
@@ -1,7 +1,10 @@
-extends Node2D
+extends Control
 
 var faction = null
+var is_current_player = false
 var color = null
 
 func _ready():
 	$Sprite.set_modulate(Color(color[0], color[1], color[2]))
+	if is_current_player:
+		$Sprite.set_scale(Vector2(1.5,1.5))
