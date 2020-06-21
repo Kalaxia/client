7aaceba5040cea3727ba8e03f89bf96f671c3b84
ChickenStorm <tetras.aigle@yahoo.fr>
2020-06-21 19:41:38 +0200

    support for windows resizing

--- menu/menu_credits.gd
+++ menu/menu_credits.gd
@@ -3,9 +3,9 @@ extends Control
 signal scene_requested(scene)
 
 func _ready():
-	$CenterContainer/VBoxContainer/BackMainMenu.connect("pressed",self,"_on_back_to_main_menu")
-	$CenterContainer/VBoxContainer/GridContainer/LinkButtonKalaxia.connect("pressed",self,"_on_link_button_press",["https://kalaxia.org"])
-	$CenterContainer/VBoxContainer/GridContainer/LinkButtonDiscord.connect("pressed",self,"_on_link_button_press",["https://discord.com/invite/mDTms5c"])
+	$Forground/CenterH/CenterV/CreditsContainer/BackMainMenu.connect("pressed",self,"_on_back_to_main_menu")
+	$Forground/CenterH/CenterV/CreditsContainer/GridContainer/LinkButtonKalaxia.connect("pressed",self,"_on_link_button_press",["https://kalaxia.org"])
+	$Forground/CenterH/CenterV/CreditsContainer/GridContainer/LinkButtonDiscord.connect("pressed",self,"_on_link_button_press",["https://discord.com/invite/mDTms5c"])
 
 func _on_back_to_main_menu():
 	emit_signal("scene_requested","menu")
