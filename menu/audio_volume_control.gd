f106baea7744d521db573519589a6144662f164d
ChickenStorm <tetras.aigle@yahoo.fr>
2020-06-25 16:07:30 +0200

    translation support

--- menu/audio_volume_control.gd
+++ menu/audio_volume_control.gd
@@ -8,13 +8,14 @@ func _ready():
 	_refersh_data()
 
 func _refersh_data():
-	$HBoxContainer/BusName.text = AudioServer.get_bus_name(bus_id)
+	$HBoxContainer/BusName.text = tr("audio.bus.%s" % AudioServer.get_bus_name(bus_id))
 	var value_volume = db2linear(AudioServer.get_bus_volume_db(bus_id))
 	$HBoxContainer/VolumeSlider.value = value_volume
 	set_volume_lable(value_volume)
 
 func set_volume_lable(value_volume):
-	$HBoxContainer/volume.text = "%3.3d %s (%7.2f dB)" % [floor(value_volume * 100),"%",linear2db(value_volume)]  
+	# todo change to a key
+	$HBoxContainer/volume.text = tr("Volume : %3.3d %s (%7.2f dB)") % [floor(value_volume * 100),"%",linear2db(value_volume)]  
 
 func _on_value_changed(value_slider):
 	var value_volume = value_slider if value_slider != 0 else db2linear(Utils.AUDIO_VOLUME_DB_MIN)
@@ -29,5 +30,5 @@ func set_bus_id(new_id : int):
 	if name == "" || name == null:
 		return
 	bus_id = new_id
-	$HBoxContainer/BusName.text = name
+	$HBoxContainer/BusName.text =  tr("audio.bus.%s" % name )
 	$HBoxContainer/VolumeSlider.value = db2linear(AudioServer.get_bus_volume_db(bus_id))
